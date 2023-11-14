fb798921ad9dcbc3c82054b5fe49f4ff7e4002e1
motazdev <motazessamdev@gmail.com>
2023-11-14 20:07:06 +0200

    debugging token headers

--- src/middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware.js
@@ -8,6 +8,7 @@ export const isAuthenticated = asyncHandler(async (req, res, next) => {
   let token = req.headers["token"] || req.cookies.jwt;
   // req.cookies.jwt ? token =`${process.env.BEARER_KEY}${token}` : token
   console.log("req.headers ::::::::::, ", req.headers);
+  console.log("req.TOKENNNN >>>>>>>>>, ", req.headers["token"]);
   if (!token || !token.startsWith(process.env.BEARER_KEY))
     return next(new Error("Valid token is required"));
 
