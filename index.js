8bd48d0f1be703877b0298057b4fd59bdda50c42
motazdev <motazessamdev@gmail.com>
2023-11-29 15:27:55 +0200

    cors update

--- index.js
+++ index.js
@@ -8,16 +8,37 @@ const app = express();
 
 app.use(cookieParser());
 dotenv.config();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:8000",
-      "https://route-ecommerce-react.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:8000",
+//       "https://route-ecommerce-react.vercel.app",
+//     ],
+//     credentials: true,
+//   })
+// );
+app.use(function (req, res, next) {
+  var allowedDomains = [
+    "http://localhost:8000",
+    "https://route-ecommerce-react.vercel.app",
+  ];
+  var origin = req.headers.origin;
+  if (allowedDomains.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
 const port = process.env.PORT;
 
 connectDB();
