f04faebc28538097adf63f58b2a2de27f102fad9
motazdev <motazessamdev@gmail.com>
2023-11-29 17:01:45 +0200

    cors update v2

--- src/utils/sendEmails.js
+++ src/utils/sendEmails.js
@@ -1,29 +1,28 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ attachments, to, html, subject }) => {
-    const transporter = nodemailer.createTransport({
-        host: "localhost",
-        port: 465,
-        secure: true,
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_APP_PASSWORD
-        }
+  const transporter = nodemailer.createTransport({
+    host: "https://route-ecommerce-react.vercel.app",
+    port: 465,
+    secure: true,
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_APP_PASSWORD,
+    },
+  });
+  async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: `"Ecommerce App 🛍️" <${process.env.EMAIL}>`,
+      to,
+      subject,
+      html,
+      attachments,
     });
-    async function main() {
-        // send mail with defined transport object
-        const info = await transporter.sendMail({
-            from: `"Ecommerce App 🛍️" <${process.env.EMAIL}>`,
-            to,
-            subject,
-            html,
-            attachments
-        });
 
-        console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
+  }
 
-    }
-
-    main().catch(console.error);
+  main().catch(console.error);
 };
